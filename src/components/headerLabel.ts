a3cb2236190e47cc87eee15f8406e67a61cd06f5
Marcos Marques <marcosmarquesdev@gmail.com>
2021-09-20 15:11:28 -0300

    Create headerlabel component

---
+++ src/components/headerLabel.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export default styled.h2`
+  font-size: 3.5rem;
+  text-transform: uppercase;
+  font-weight: 700;
+  background-image: linear-gradient(
+    to right,
+    ${(props) => props.theme.colors.lightGreen},
+    ${(props) => props.theme.colors.darkGreen}
+  );
+  display: inline-block;
+  background-clip: text;
+  color: transparent;
+  letter-spacing: 0.2rem;
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.02);
+    text-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.3);
+  }
+`;
